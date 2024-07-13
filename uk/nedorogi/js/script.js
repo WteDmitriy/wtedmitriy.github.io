0c45f05c293ee2394fab9e1f7e3cb2253f9c7bda
WteDmitriy <66473481+WteDmitriy@users.noreply.github.com>
2024-07-13 22:45:48 +0300

    Add files via upload

---
+++ uk/nedorogi/js/script.js
@@ -0,0 +1,59 @@
+function filterCatalog(filterItemClass) {
+    filterItem = document.querySelectorAll('.box');
+    document.querySelector(filterItemClass).addEventListener('click', event => {
+        if (event.target.tagName !== 'OPTION') return false;
+
+        let filterClass = event.target.dataset['f'];
+
+        filterItem.forEach( elem => {
+            elem.classList.remove('hide');
+            if (!elem.classList.contains(filterClass) && filterClass!== 'all') {
+                elem.classList.add('hide');
+            }
+        });
+    });
+};
+
+
+//Кнопка с выпадающим списком
+document.querySelectorAll(".dropbutton__btn").forEach(button => {
+    button.addEventListener('click', function(event){
+        document.querySelectorAll('.dropbutton__item').forEach(item => {
+            if(item.querySelector('.dropbutton__btn')!==button){
+                item.classList.remove('dropbutton__item-open')
+            }    
+        })
+        event.isClick = true
+        button.parentElement.classList.toggle('dropbutton__item-open')
+    })
+})
+document.body.addEventListener('click', function(event){
+    if (
+        event._isClick == true ||
+        event.target.classList.contains('dropbutton__btn') == true ||
+        event.target.classList.contains('dropdown-menu') == true   
+        ) return
+
+        document.querySelectorAll('.dropbutton__item').forEach(item => {
+            item.classList.remove('dropbutton__item-open')
+    })
+})
+
+//Гамбургер
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
